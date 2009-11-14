2d363f7ca53d03083e6ef06ab96942d2fe8ef062
ppr.vitaly <ppr.vitaly@04576334-096a-11de-9036-69e38a880166>
2009-11-14 19:51:57 +0000

    added comments to samples
    git-svn-id: https://django-reporting.googlecode.com/svn/trunk@6 04576334-096a-11de-9036-69e38a880166

--- samples/people_example/people/reports.py
+++ samples/people_example/people/reports.py
@@ -5,33 +5,33 @@ from models import Person
 class PersonReport(reporting.Report):
     model = Person
     verbose_name = 'Person Report'
-    annotate = (
-        ('id', Count, 'Total'),
-        ('salary', Sum),
+    annotate = (                    # Annotation fields (tupples of field, func, title)
+        ('id', Count, 'Total'),     # example of custom title for column 
+        ('salary', Sum),            # no title - column will be "Salary Sum"
         ('expenses', Sum),
     )
-    aggregate = (
+    aggregate = (                   # columns that will be aggregated (syntax the same as for annotate)
         ('id', Count, 'Total'),
         ('salary', Sum, 'Salary'),
         ('expenses', Sum, 'Expenses'),
     )
-    group_by = [
+    group_by = [                   # list of fields and lookups for group-by options
         'department',
         'department__leader', 
         'occupation', 
     ]
-    list_filter = [
+    list_filter = [                # This are report filter options (similar to django-admin)
        'occupation',
        'country',
     ]
     
-    detail_list_display = [
+    detail_list_display = [        # if detail_list_display is defined user will be able to see how rows was grouped  
         'name', 
         'salary',
         'expenses', 
     ]
 
-    date_hierarchy = 'birth_date'
+    date_hierarchy = 'birth_date' # the same as django-admin
 
 
-reporting.register('people', PersonReport)
+reporting.register('people', PersonReport) # Do not forget to 'register' your class in reports
